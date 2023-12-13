cc7da21388b4034f551bf781f9b6d12b50a891ea
Cornelius Linux <radi.adi78@gmail.com>
2023-12-14 04:25:13 +0700

    Refactor top bar

--- app/src/main/java/com/xored/view/TopBar.kt
+++ app/src/main/java/com/xored/view/TopBar.kt
@@ -1,8 +1,13 @@
 package com.xored.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
@@ -14,6 +19,7 @@ import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.NavigationDrawerItemDefaults
@@ -21,7 +27,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.xored.model.NavigationItem
 import com.xored.viewmodel.MainMenuViewModel
 
@@ -51,41 +60,25 @@ fun DrawerSheet(
     viewModel: MainMenuViewModel,
     onDrawerItemClick: () -> Unit
 ) {
-    val items = listOf(
-        NavigationItem(
-            title = "All",
-            selectedIcon = Icons.Filled.Home,
-            unselectedIcon = Icons.Outlined.Home
-        ),
-        NavigationItem(
-            title = "Urgent",
-            selectedIcon = Icons.Filled.Info,
-            unselectedIcon = Icons.Outlined.Info
-        ),
-        NavigationItem(
-            title = "Settings",
-            selectedIcon = Icons.Filled.Settings,
-            unselectedIcon = Icons.Outlined.Settings
-        )
-    )
-
     ModalDrawerSheet {
-        Spacer(modifier = Modifier.height(16.dp))
-        items.forEachIndexed{ index, item ->
-            NavigationDrawerItem(
-                label = { Text(text = item.title) },
-                selected = index == viewModel.selectedItemIndex,
-                onClick = onDrawerItemClick.also {
-                    viewModel.selectedItemIndex = index
-                },
-                icon = {
-                    Icon(
-                        imageVector = if(index == viewModel.selectedItemIndex) item.selectedIcon
-                        else item.unselectedIcon,
-                        contentDescription = item.title)
-                },
+        Box(modifier = Modifier
+            .background(Color.Gray)
+            .fillMaxWidth()
+        ) {
+            Column(
                 modifier = Modifier
-                    .padding(NavigationDrawerItemDefaults.ItemPadding))
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "TUGAS BESAR MATEMATIKA DISKRIT",
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                )
+                Text(text = "Nama: Cornelius Linux")
+                Text(text = "NIM: 122140079")
+            }
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        
     }
 }
