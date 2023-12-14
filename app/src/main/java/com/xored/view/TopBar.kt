fc7285b7ef1d9dd2dc2d766786d9e6952cc44de4
Cornelius Linux <radi.adi78@gmail.com>
2023-12-14 18:06:18 +0700

    Refactor code

--- app/src/main/java/com/xored/view/TopBar.kt
+++ app/src/main/java/com/xored/view/TopBar.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.xored.viewmodel.MainMenuViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
